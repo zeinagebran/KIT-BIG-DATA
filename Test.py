7b7805e82756a494d789445da9e612c7447c02ef
zeina <zeina.gebran@net.usj.edu.lb>
2024-09-26 20:33:58 +0200

    git commit -m "Ajout du fichier 'App.py' et changement du 'Test.py' file pour lire la dataset"

--- Test.py
+++ Test.py
@@ -1,1 +1,17 @@
-print("hello")
+import zipfile
+import os
+import pandas as pd
+
+# Remplacez par le chemin réel
+
+zip_file_path = "C:\\Users\\User\\Desktop\\MASTERE SPECIALISE IA\\KIT BIG DATA BGDIA700\\archive.zip"
+
+
+with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+    zip_ref.extractall("extracted_data")
+
+recipes_df = pd.read_csv("extracted_data/PP_recipes.csv")
+interactions_df = pd.read_csv("extracted_data/interactions_test.csv")
+
+print(recipes_df.head())
+print(interactions_df.head())
---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from Test import recipes_df
+
+recipe_id = st.selectbox("Choisissez une recette :",
+                         recipes_df['id'].unique())
+selected_recipe = recipes_df[recipes_df['id'] == recipe_id]
+st.write(selected_recipe)
