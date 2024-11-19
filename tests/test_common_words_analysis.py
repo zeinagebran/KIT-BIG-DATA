9a8bab164f81f7b38f2b596224cfea7609ec5a7e
mlarnouhet <mlarnouhet@gmail.com>
2024-11-19 08:52:06 +0100

    test

--- tests/test_common_words_analysis.py
+++ tests/test_common_words_analysis.py
@@ -46,6 +46,23 @@ def test_compute_top_keywords(sample_recipes, sample_interactions):
         wordcloud, WordCloud), "compute_top_keywords should return a WordCloud object."
 
 
+def test_common_words_with_empty_recipe_names(sample_interactions):
+    # Recipe name list with empty strings
+    recipes_with_empty_names = pd.DataFrame({
+        'id': [1, 2, 3],
+        'name': ['', 'Recipe without ingredients', ''],
+        'submitted': ['2024-01-01', '2024-01-15', '2024-02-15'],
+        'ingredients': ['', 'eggs, milk', '']
+    })
+    top_recipes_analysis = TopRecipesAnalysis(recipes_with_empty_names, sample_interactions)
+    top_recipes = top_recipes_analysis.display_popular_recipes_and_visualizations(return_top_recipes=True, mcw_flag=True)
+    analysis = CommonWordsAnalysis(top_recipes)
+    text = analysis.compute_top_keywords()
+    wordcloud = analysis.display_wordcloud(text)
+    assert isinstance(
+        wordcloud, WordCloud), "Expected a WordCloud with empty recipe names"
+
+
 #def test_common_words_analysis_edge_case_ratings(config, sample_recipes, sample_interactions):
 #    # Test for recipes with ratings at the boundary of min_rating
 #    analysis = CommonWordsAnalysis(sample_recipes, sample_interactions, config)
@@ -55,17 +72,4 @@ def test_compute_top_keywords(sample_recipes, sample_interactions):
 #        wordcloud, WordCloud), "Expected a WordCloud object even with boundary rating recipes"
 #
 #
-#def test_common_words_with_empty_recipe_names(config, sample_interactions):
-#    # Recipe name list with empty strings
-#    recipes_with_empty_names = pd.DataFrame({
-#        'id': [1, 2, 3],
-#        'name': ['', 'Recipe without ingredients', ''],
-#        'submitted': ['2024-01-01', '2024-01-15', '2024-02-15'],
-#        'ingredients': ['', 'eggs, milk', '']
-#    })
-#    analysis = CommonWordsAnalysis(
-#        recipes_with_empty_names, sample_interactions, config)
-#    analysis.format_recipe(2024)
-#    wordcloud = analysis.compute_top_keywords()
-#    assert isinstance(
-#        wordcloud, WordCloud), "Expected a WordCloud with empty recipe names"
+
