bf12a0d190c126b40b252ba85d1e7bc94be14ece
mlarnouhet <mlarnouhet@gmail.com>
2024-12-03 22:28:35 +0100

    added logger test

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,10 +1,12 @@
 import pytest
 import logging
+from src.optimRecipes.functions import prepare_directories
 from src.optimRecipes.logger import Logger
 from src.optimRecipes.config import Config
 
 def test_logger():
     cfg = Config()
+    prepare_directories(cfg=cfg)
     log_module = Logger(cfg=cfg)
     assert isinstance(
         log_module.logger, logging.Logger), "log_module should have a logger object."
