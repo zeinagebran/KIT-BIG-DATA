de5a377ebb5270accef08bc505f61eeea252968b
Nicolas Allègre <nicolas.allegre@telecom-paris.fr>
2024-12-03 10:07:12 +0100

    Add docstring module, class and method for analysis_top_15.py.

--- src/optimRecipes/analysis_top_15.py
+++ src/optimRecipes/analysis_top_15.py
@@ -1,16 +1,36 @@
+"""optimRecipes/analysis_top_15.py file.
+
+Sub-module Streamlit for page top_50_analysis.
+
+"""
+###############################################################################
+# IMPORTS :
+# /* Standard includes. */
+# /* Extern modules */
 import streamlit as st
 
+# /* Intern modules */
 from functions import TopRecipesAnalysis
 
 
+###############################################################################
+# CLASS :
 class top_50_analysis_module:
+    """Class to wrap this module functions to interact with webapp.
+
+    This class is used by webapp streamlit for the analysis of the most popularity
+    recipes.
+    """
+
     def __init__(self, recipes_df, interactions_df, log_module, cfg):
+        """Initialize the class with the data."""
         self.cfg = cfg
         self.recipes_df = recipes_df
         self.interactions_df = interactions_df
         self.log_module = log_module
 
     def run(self):
+        """Build analysis, and print result on a web streamlit page."""
         st.title("🍲 Top 15 Most Popular Recipes")
         st.markdown(
             """
