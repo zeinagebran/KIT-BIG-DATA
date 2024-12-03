3f609e4bee70a26c4ec53819cba68dd5e8f81dae
Nicolas Allègre <nicolas.allegre@telecom-paris.fr>
2024-12-03 09:41:36 +0100

    Add docstring module for main.py

--- src/optimRecipes/main.py
+++ src/optimRecipes/main.py
@@ -1,12 +1,25 @@
+"""optimRecipes/main.py file.
+
+main entry for the optimRecipes app.
+
+"""
+###############################################################################
+# IMPORTS :
+# /* Standard includes. */
+# /* Extern modules */
 import nltk
 
+# /* Intern modules */
 from app import WebApp
 from config import Config
 from functions import prepare_directories
 from logger import Logger
 
 
+###############################################################################
+# FUNCTIONS :
 def main():
+    """Entry point of the module."""
     nltk.download('stopwords')
     cfg = Config()
     prepare_directories(cfg=cfg)
