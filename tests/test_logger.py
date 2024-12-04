ace29879a9ecd63c05f5955778d63f3a0663de2c
Nicolas Allègre <nicolas.allegre@telecom-paris.fr>
2024-12-04 16:48:58 +0100

    Modify test_logger for adding test

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,12 +1,24 @@
-import pytest
 import logging
-from src.optimRecipes.functions import prepare_directories
-from src.optimRecipes.logger import Logger
-from src.optimRecipes.config import Config
+import os
+import pytest
+from pathlib import Path
+
+from optimRecipes.functions import prepare_directories
+from optimRecipes.logger import Logger
+from optimRecipes.config import Config
+
 
 def test_logger():
     cfg = Config()
     prepare_directories(cfg=cfg)
     log_module = Logger(cfg=cfg)
-    assert isinstance(
-        log_module.logger, logging.Logger), "log_module should have a logger object."
+    assert isinstance(log_module.logger,
+                      logging.Logger), "log_module should have a logger object."
+    
+    assert Path(os.path.join(log_module.cfg.logging_dir, 'app.log')).exists(),"log file should exist."
+
+    log_module.log_critical('Message level crital')
+    log_module.log_warning('Message level warning')
+    log_module.log_info('Message level info')
+    log_module.log_warning('Message level warning')
+    log_module.log_debug('Message level debug')
